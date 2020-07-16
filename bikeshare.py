11b17400e3a4e516a653bbbd08a356b5c2a408a1
Jason <jason_mar@mpaa.org>
2020-07-15 19:23:51 -0700

    style: Edit opening input statement

--- bikeshare.py
+++ bikeshare.py
@@ -16,7 +16,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello there! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     input_city = input('Please choose one of these cities: Chicago, New York City, Washington \n').lower()
     while input_city not in ['chicago', 'new york city', 'washington']:
